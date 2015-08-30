f9ae5849f0ad5394f2278f561f45c51dcc54d6fc
Ronan <ronanir600@gmail.com>
2015-08-30 23:59:54 +0100

    update

--- Controllers/index.js
+++ Controllers/index.js
@@ -4,9 +4,6 @@ var indexController = {
 	index: function(req, res) {
 		res.render('index');
 	}, 
-	// templates: function(req, res) {
-	// 	res.render('templates/forms')
-	// },
 	payment: function(req, res) {
 		res.render('payment');
 	} 
