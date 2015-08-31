5ec4e77c0597d270dc13afbe3ccaf26044610982
Ronan <ronanir600@gmail.com>
2015-08-31 16:34:50 +0100

    new

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 
 // app.get('/', indexController.index);
 app.get('/',  function(req, res) {
-	res.render('index')
+	res.render('views/index')
 })
 
 app.get('/payment', indexController.payment);
